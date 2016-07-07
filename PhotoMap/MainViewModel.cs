d9395ba5db22460e8974db71cec16993578de9f0
Steveou <steffen.krengel@gmx.de>
2016-07-07 12:31:58 +0200

    Map centers to photo location

--- PhotoMap/MainViewModel.cs
+++ PhotoMap/MainViewModel.cs
@@ -2,7 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Windows.Devices.Geolocation;
 using Windows.Storage;
@@ -13,6 +14,7 @@ namespace PhotoMap
     public class MainViewModel : ReactiveObject
     {
         public ReactiveCommand<List<Photo>> LoadPhotos { get; protected set; }
+        public ReactiveCommand<Unit> CenterToLocation { get; protected set; }
 
         private ObservableAsPropertyHelper<List<Photo>> photos;
         public List<Photo> Photos => photos.Value;
@@ -20,13 +22,47 @@ namespace PhotoMap
         private ObservableAsPropertyHelper<bool> isLoading;
         public bool IsLoading => isLoading.Value;
 
+        private Photo selectedPhoto;
+        public Photo SelectedPhoto
+        {
+            get { return selectedPhoto; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref selectedPhoto, value);
+            }
+        }
+
+        private Geopoint mapCenter;
+        public Geopoint MapCenter
+        {
+            get { return mapCenter; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref mapCenter, value);
+            }
+        }
+
+        private double mapZoomLevel;
+        public double MapZoomLevel
+        {
+            get { return mapZoomLevel; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref mapZoomLevel, value);
+            }
+        }
+
         public MainViewModel()
         {
             LoadPhotos = ReactiveCommand.CreateAsyncTask(p => GetPhotos());
             LoadPhotos.ThrownExceptions.Subscribe(ex => System.Diagnostics.Debug.WriteLine(ex));
 
+            CenterToLocation = ReactiveCommand.CreateAsyncTask(p => CenterMapToLocation());
+
             photos = LoadPhotos.ToProperty(this, x => x.Photos, new List<Photo>());
             isLoading = LoadPhotos.IsExecuting.ToProperty(this, x => x.IsLoading, false);
+
+            this.WhenAnyValue(x => x.SelectedPhoto).InvokeCommand(CenterToLocation);
         }
 
         /// <summary>
@@ -67,5 +103,19 @@ namespace PhotoMap
             return photosList;
         }
 
+        /// <summary>
+        /// Center the map to the position of the selected photo.
+        /// </summary>
+        /// <returns></returns>
+        private Task<Unit> CenterMapToLocation()
+        {
+            if (SelectedPhoto != null)
+            {
+                MapCenter = SelectedPhoto.Location;
+                MapZoomLevel = 16;
+            }
+
+            return Task.FromResult(Unit.Default);
+        }
     }
 }
